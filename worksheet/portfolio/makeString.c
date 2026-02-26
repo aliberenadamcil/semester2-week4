6319a14db9b10de5f0f98edc1d9f36c0276c8ca2
aliberenadamcil <berenadamcil@gmail.com>
2026-02-26 11:31:19 +0000

    .

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -1,7 +1,7 @@
 
 /*
-Name:
-Student ID:
+Name: Ali Beren Adamcil
+Student ID:202018616
 */
 
 #include <stdio.h>
@@ -10,9 +10,17 @@ Student ID:
 int main( int argc, char **argv ) {
 
     // define appropriate data to hold your answer
+     char buffer[50];
+     int i ;
+
+     strcpy(buffer,argv[1]); 
 
     // process the command-line data using appropriate string functions
 
+        for(i=2; i<argc; i++){
+            strcat(buffer,"-");
+            strcat(buffer,argv[i]);
+        }
     printf("%s\n",buffer); // use only this single print statement in your submitted code
 
     return 0;
